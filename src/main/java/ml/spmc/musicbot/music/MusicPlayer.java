906bcd883d9721fe3fd0c6fa7a8dee7702c6afb4
tcfplayz <58693033+tcfplayz@users.noreply.github.com>
2024-07-13 20:52:53 +0800

    2.5.2 - use new yt api

--- src/main/java/ml/spmc/musicbot/music/MusicPlayer.java
+++ src/main/java/ml/spmc/musicbot/music/MusicPlayer.java
@@ -5,6 +5,7 @@ import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.source.AudioSourceManagers;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
@@ -30,11 +31,14 @@ public class MusicPlayer {
     public static void playMusic() {
         VoiceChannel channel = bot.getVoiceChannelById(Config.MUSIC_CHANNEL_ID);
         assert guild != null;
+        dev.lavalink.youtube.YoutubeAudioSourceManager ytSourceManager = new dev.lavalink.youtube.YoutubeAudioSourceManager();
+        manager.registerSourceManager(ytSourceManager);
+
         final AudioManager manager2 = guild.getAudioManager();
         manager2.setSendingHandler(musicManager.getSendHandler());
         if (!manager2.isConnected()) {
             manager2.openAudioConnection(channel);
-            AudioSourceManagers.registerRemoteSources(manager);
+            AudioSourceManagers.registerRemoteSources(manager, YoutubeAudioSourceManager.class);
             AudioSourceManagers.registerLocalSource(manager);
             manager2.setSelfDeafened(true);
             player.setVolume(100);
