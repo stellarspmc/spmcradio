f6aecbb9d32ce44accb28999106a5c68f6ca5613
tcfplayz <58693033+tcfplayz@users.noreply.github.com>
2024-09-27 18:57:03 +0800

    2.5.5a - features

--- src/main/java/fun/spmc/radio/music/MusicType.java
+++ src/main/java/fun/spmc/radio/music/MusicType.java
@@ -2,8 +2,8 @@ package fun.spmc.radio.music;
 
 public enum MusicType {
     DEFAULT("https://www.youtube.com/playlist?list=PL7AMJQQdFhwYlRIt-SmDsIVZ_xpJYWvgC"),
-    NICKWONG("https://www.youtube.com/playlist?list=PL7AMJQQdFhwYLhwgHJPlnI53m9j297DSD"),
-    JAKEWONG("https://www.youtube.com/playlist?list=PL7AMJQQdFhwbOjZFzretFFciOfcOMdP9O"),
+    NICK("https://www.youtube.com/playlist?list=PL7AMJQQdFhwYLhwgHJPlnI53m9j297DSD"),
+    JAKE("https://www.youtube.com/playlist?list=PL7AMJQQdFhwbOjZFzretFFciOfcOMdP9O"),
     PUBLIC("https://www.youtube.com/playlist?list=PL7AMJQQdFhwas09hzDRFI9oHqjPpUM4CN"),
     SHARK(" https://youtube.com/playlist?list=PLd-uSADIv6DDKdFy6-3PyRc2CBgUnIn1r");
 
